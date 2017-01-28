3bf7014f7c2851a8cd0da8291214fec62dc20b6f
laggingreflex <laggingreflex@gmail.com>
2017-01-28 16:52:31 +0530

    fix: make it work with start-debounce/16

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-module.exports = (opts = {}, callback) => (input) => {
+module.exports = (files, opts = {}) => (callback) => () => {
   return function watchDebounce(log, reporter) {
     const Start = require('start').default;
     const watch = require('start-watch').default;
     const debounce = require('debounce-queue');
 
-    if (!callback && typeof opts === 'function') {
-      callback = opts;
-      opts = {};
-    }
-
-    if (typeof opts === 'string' || Array.isArray(opts)) {
-      opts = { files: opts };
-    }
-
-    if (input && input.length && !opts.files) {
-      opts.files = input;
-    }
-
     const start = new Start(reporter);
 
-    const debouncedCallback = debounce(callback, Object.assign({}, opts, {
-      enqueue(data, queue, defaultEnqueue) {
-        // because start-watch returns `[ [ ...files ] ]` (nested array) on the first run
-        let ret
-        if (Array.isArray(data)) {
-          ret = queue.concat(data.filter(d => !queue.includes(d)));
-        } else {
-          ret = defaultEnqueue();
-        }
-        if (opts.enqueue) {
-          ret = opts.enqueue(data, ret, defaultEnqueue)
-        }
-        return ret;
-      }
-    }));
+    return new Promise((resolve, reject) => {
 
-    return start(watch(opts, debouncedCallback));
+      const callbackWrapper = (...args) => {
+        callback(...args).then(resolve, reject);
+      };
+
+      const debounced = debounce(callbackWrapper, Object.assign({}, opts, {
+        enqueue(data, queue, defaultEnqueue) {
+          // because start-watch returns `[ [ ...files ] ]` (nested array) on the first run
+          let ret
+          if (Array.isArray(data)) {
+            ret = queue.concat(data.filter(d => !queue.includes(d)));
+          } else {
+            ret = defaultEnqueue();
+          }
+          if (opts.enqueue) {
+            ret = opts.enqueue(data, ret, defaultEnqueue)
+          }
+          return ret;
+        }
+      }));
+
+      const debouncedCallback = (...args) => new Promise((resolve, reject) => {
+        debounced(...args);
+        resolve();
+      });
+
+      return start(watch(files, opts)(debouncedCallback));
+    });
   };
 };
